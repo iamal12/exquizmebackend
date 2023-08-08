e391bd5e17b472d963fd195a9861b15af93d1130
amalaugustine <amaledassery@gmail.com>
2023-08-08 12:41:27 +0530

    Merge remote changes into main branchsddd

--- src/routes/fixture.js
+++ src/routes/fixture.js
@@ -3,6 +3,55 @@ const router13 = express.Router();
 const mysqlConnection = require('../database/database');
 
 // Create a new fixture
+/**
+ * @swagger
+ * tags:
+ *   name: Fixtures
+ *   description: Fixture management endpoints
+ */
+
+/**
+ * @swagger
+ * /api/fixtures:
+ *   post:
+ *     summary: Create a new fixture
+ *     tags: [Fixtures]
+ *     requestBody:
+ *       description: Fixture data
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               round_id:
+ *                 type: integer
+ *               player1_id:
+ *                 type: integer
+ *               player2_id:
+ *                 type: integer
+ *               player1_score:
+ *                 type: integer
+ *               player2_score:
+ *                 type: integer
+ *             example:
+ *               round_id: 1
+ *               player1_id: 123
+ *               player2_id: 456
+ *               player1_score: 3
+ *               player2_score: 2
+ *     responses:
+ *       '201':
+ *         description: Fixture created successfully
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Fixture created successfully
+ *               fixture_id: 789
+ *       '500':
+ *         description: Internal Server Error
+ */
+
 router13.post('/fixtures', (req, res) => {
     const { round_id, player1_id, player2_id, player1_score, player2_score } = req.body;
 
@@ -21,6 +70,24 @@ router13.post('/fixtures', (req, res) => {
 });
 
 // Get all fixtures
+/**
+ * @swagger
+ * /api/fixtures:
+ *   get:
+ *     summary: Get all fixtures
+ *     tags: [Fixtures]
+ *     responses:
+ *       '200':
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Fixture'
+ *       '500':
+ *         description: Internal Server Error
+ */
 router13.get('/fixtures', (req, res) => {
     mysqlConnection.query('SELECT * FROM fixture', (error, rows, fields) => {
         if (!error) {
@@ -33,6 +100,31 @@ router13.get('/fixtures', (req, res) => {
 });
 
 // Get fixture by ID
+/**
+ * @swagger
+ * /api/fixtures/{id}:
+ *   get:
+ *     summary: Get fixture by ID
+ *     tags: [Fixtures]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: Fixture ID
+ *     responses:
+ *       '200':
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Fixture'
+ *       '404':
+ *         description: Fixture not found
+ *       '500':
+ *         description: Internal Server Error
+ */
 router13.get('/fixtures/:id', (req, res) => {
     const id = req.params.id;
     mysqlConnection.query('SELECT * FROM fixture WHERE fixture_id = ?', [id], (error, rows, fields) => {
@@ -50,6 +142,39 @@ router13.get('/fixtures/:id', (req, res) => {
 });
 
 // Update fixture by ID
+/**
+ * @swagger
+ * /api/fixtures/{id}:
+ *   put:
+ *     summary: Update fixture by ID
+ *     tags: [Fixtures]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: Fixture ID
+ *     requestBody:
+ *       description: Updated fixture data
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Fixture'
+ *     responses:
+ *       '200':
+ *         description: Fixture updated successfully
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Fixture updated successfully
+ *       '404':
+ *         description: Fixture not found
+ *       '500':
+ *         description: Internal Server Error
+ */
+
 router13.put('/fixtures/:id', (req, res) => {
     const id = req.params.id;
     const { round_id, player1_id, player2_id, player1_score, player2_score } = req.body;
@@ -73,6 +198,32 @@ router13.put('/fixtures/:id', (req, res) => {
 });
 
 // Delete fixture by ID
+/**
+ * @swagger
+ * /api/fixtures/{id}:
+ *   delete:
+ *     summary: Delete fixture by ID
+ *     tags: [Fixtures]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: Fixture ID
+ *     responses:
+ *       '200':
+ *         description: Fixture deleted successfully
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Fixture deleted successfully
+ *       '404':
+ *         description: Fixture not found
+ *       '500':
+ *         description: Internal Server Error
+ */
+
 router13.delete('/fixtures/:id', (req, res) => {
     const id = req.params.id;
     mysqlConnection.query('DELETE FROM fixture WHERE fixture_id = ?', [id], (error, result) => {
