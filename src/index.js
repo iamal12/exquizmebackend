54e6c8ca68d7b91c9628c074aa2a2342ac9ab242
amalaugustine <amaledassery@gmail.com>
2023-08-08 11:57:40 +0530

    Merge remote changes into main branchsddd

--- src/index.js
+++ src/index.js
@@ -13,10 +13,10 @@ const quizrooms = require('./routes/quizroom.js');
 const quizroomparticipant = require('./routes/quizroomparticipant.js');
 const playersRouter = require('./routes/player.js');
 const rounds = require('./routes/rounds.js');
-//const fixtures = require('./routes/fixture.js');
-//const randomquizbattle = require('./routes/randomquizbattles.js');
-//const onetoonequizchallenge = require('./routes/onetoonequizchallenge.js');
-//const onetoonequizresponses = require('./routes/onetoonequizresponses.js');
+const fixtures = require('./routes/fixture.js');
+const randomquizbattle = require('./routes/randomquizbattles.js');
+const onetoonequizchallenge = require('./routes/onetoonequizchallenge.js');
+const onetoonequizresponses = require('./routes/onetoonequizresponses.js');
 // Middlewares
 app.use(express.json());
 app.use(cors());
@@ -34,10 +34,10 @@ app.use('/quizrooms', quizrooms);
 app.use('/quizroomparticipants', quizroomparticipant);
 app.use('/players', playersRouter);
 app.use('/rounds', rounds);
-//app.use('/fixtures', fixtures);
-//app.use('/quiz_battles', randomquizbattle);
-//app.use('/onetoquizchallenges', onetoonequizchallenge);
-//app.use('/onetoquizresponses', onetoonequizresponses);
+app.use('/fixtures', fixtures);
+app.use('/quiz_battles', randomquizbattle);
+app.use('/onetoquizchallenges', onetoonequizchallenge);
+app.use('/onetoquizresponses', onetoonequizresponses);
 
 // Starting the server
 const port = 3000; // Set your desired port number here
